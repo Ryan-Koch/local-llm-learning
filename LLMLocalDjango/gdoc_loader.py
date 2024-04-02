d33c848553c722b21fb01f51a353fc88fff815c9
Ryan Koch <4325613+Ryan-Koch@users.noreply.github.com>
2024-04-02 17:52:12 -0400

    Didn't get anywhere with summaries yet, but this adds incremental improvements overall to things.

--- LLMLocalDjango/gdoc_loader.py
+++ LLMLocalDjango/gdoc_loader.py
@@ -2,8 +2,6 @@ import configparser
 import os
 
 from langchain_community.document_loaders import GoogleDriveLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.llms import Ollama
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_experimental.text_splitter import SemanticChunker
 
@@ -23,10 +21,10 @@ credential_path = gdoc_config.get("gdoc_settings", "credential_path")
 
 
 def main():
-    # This looks like it shouldn't be needed. But it is, even if the value isn't important. More in this GH issue: https://github.com/langchain-ai/langchain/issues/14725.
+    # This looks like it shouldn't be needed. But it is, even if the value isn't important. More in this GH issue:
+    # https://github.com/langchain-ai/langchain/issues/14725.
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ""
 
-    print("Loading Google Drive documents...")
     loader = GoogleDriveLoader(
         folder_id=folder_id,
         credentials_path=credential_path,
@@ -37,19 +35,23 @@ def main():
         recursive=False,
     )
     # Get all the stuff in the gdocs folder
-    data = loader.load()
+    print("Downloading documents...")
+    gdocs = loader.load()
 
     # Splitting the docs up
     print("Splitting documents...")
-    text_splitter = SemanticChunker(OllamaEmbeddings(base_url=base_url, model="llama2"))
-    # Okay, so if you don't do this locally you will get an index error when this runs: https://github.com/langchain-ai/langchain/compare/master...psaegert:langchain:master
-    # hopefully this or something similar will appear in a PR on the library repo soon.
-    documents = text_splitter.split_documents(data)
+    text_splitter = SemanticChunker(OllamaEmbeddings(base_url=base_url, model="llama2"),)
+    # Okay, so if you don't do this locally you will get an index error when this runs:
+    # https://github.com/langchain-ai/langchain/compare/master...psaegert:langchain:master hopefully this or
+    # something similar will appear in a PR on the library repo soon.
+    splits_for_store = text_splitter.split_documents(gdocs)
 
     # Initialize the vector store
     print("Initializing vector store...")
     vs = vector_store.VectorStore()
-    vs.create_store(documents, OllamaEmbeddings(base_url=base_url, model="llama2"))
+    vs.create_store(
+        splits_for_store, OllamaEmbeddings(base_url=base_url, model="llama2")
+    )
 
     print("Done!")
     return True
